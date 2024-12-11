3eb72eda6801c8d1a1e8b84dcf192ab36e7f0d59
kellisha2 <kellisha.andrew2@gmail.com>
2024-12-11 01:36:32 -0500

    html

---
+++ index.js
@@ -0,0 +1,24 @@
+// import "./styles.css";
+
+const title = document.getElementById("header");
+
+title.innerHTML = "<h1>Symbol of Freedom: Haiti's Soup Joumou</h1>";
+
+
+const body = document.body
+
+const newDiv = document.createElement('div');
+
+// newDiv.id = "history";
+newDiv.classList.add("history");
+
+
+body.appendChild(div)
+
+
+const ingredientList = document.getElementById("ingredients");
+
+
+
+
+
