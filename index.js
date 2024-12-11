9b94d6801b41f3aa6a31d32189ac8c0b3a3ccfce
kellisha2 <kellisha.andrew2@gmail.com>
2024-12-11 02:32:38 -0500

    created an element

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ const body = document.body
 
 const newDiv = document.createElement('div');
 
-// newDiv.id = "history";
-newDiv.classList.add("history");
+newDiv.id = "history";
+// newDiv.classList.add("history");
+console.log(newDiv)
+newDiv.innerHTML = "<h1>The History of Soup Joumou</h1><p>Every New Year’s Day, Haitians around the world consume soup joumou as a way to commemorate Haitian Independence Day. On January 1, 1804, Haitians declared independence from French colonial rule following the Haitian Revolution that began in 1791.</p>"
+
+body.appendChild(newDiv)
 
 
-body.appendChild(div)
+// const ingredientList = document.getElementById("ingredients");
 
 
-const ingredientList = document.getElementById("ingredients");
-
-
+const divStyle = document.querySelectorAll(div)
 
 
 
